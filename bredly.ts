465b520d068dd0b498394c16078dfc140284ad49
badunius <badunius@gmail.com>
2021-07-19 23:45:03 +0300

    initial commit, duh

---
+++ bredly.ts
@@ -0,0 +1,29 @@
+type TDictionary = {
+	[key: string]: string[]
+}
+
+const pick = (list: string[] = []): string => list[Math.floor(Math.random() * list.length)]
+
+const parse = (template: string = '', dictionary: TDictionary = {}): string => {
+	// find all replacements
+	const replacements = template.match(/\$[a-zA-Z0-9]*/) || []
+
+	// replace them with the coresponding entries in dictionary
+	let output = template
+	replacements.forEach(key => {
+		if (key in dictionary) {
+			const option = parse(pick(dictionary[key]), dictionary)
+			output = output.replace(key, option)
+		}
+	})
+
+	return output
+}
+
+const compose = (dictionary: TDictionary = {}, entryPoint: string): string => parse(pick(dictionary[entryPoint]), dictionary)
+
+// console.log(compose({
+// 	'$quality': ['red', 'sour', 'ripe', 'rotten'],
+// 	'$object': ['a piano', 'a $quality apple'],
+// 	'$start': ['Subject is $subject', '$object is an object']
+// },'$start'))
